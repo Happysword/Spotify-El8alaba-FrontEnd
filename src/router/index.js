d2c97e21c9b911fcb77995eb27e961931cbe57da
ahmedx3 <ahmed.moustafaa99@gmail.com>
2020-02-29 23:34:02 +0200

    Made Layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,30 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Main from '../views/MainPage/Main.vue';
+import Home from '../views/MainPage/Home.vue';
+import SignUp from '../views/Authentication/SignUp.vue';
+import LogIn from '../views/Authentication/LogIn.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'Main',
+    component: Main,
+    children: [
+      { path: '/', name: 'home', component: Home },
+    ],
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp,
+  },
+  {
+    path: '/login',
+    name: 'LogIn',
+    component: LogIn,
   },
 ];
 
---
+++ src/views/MainPage/Main.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <v-content class="grey darken-4" color="#282828">
+
+      <Navbar></Navbar>
+      <router-view></router-view>
+
+    </v-content>
+
+</template>
+
+<script>
+import Navbar from '../../components/NavBar.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Navbar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
