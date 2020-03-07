7da1fe4b147be0aeee34d7a7ac5c46bbb19e06a0
Happysword <45149700+Happysword@users.noreply.github.com>
2020-03-07 22:01:57 +0200

    Modified TopBar View

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -10,24 +10,65 @@
 
     <v-spacer></v-spacer>
 
-    <v-btn rounded depressed outlined class="mx-4" route to="/signup"
-      >SIGN UP</v-btn
+    <v-btn rounded depressed large outlined class="mx-2" route to="/premium"
+      >Upgrade</v-btn
     >
 
-    <v-btn
-      rounded
-      depressed
-      color="white black--text"
-      class="mx-4"
-      route
-      to="/login"
-      >LOG IN</v-btn
-    >
+    <div class="text-center">
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn
+            rounded
+            depressed
+            color="grey darken-4"
+            v-on="on"
+            class="mx-4 text-user"
+            ><v-avatar size="35"
+              ><v-img :src="UserInfo.photo"></v-img
+            ></v-avatar>
+            <div>
+              {{ UserInfo.name }}
+            </div>
+            <v-avatar size="20">
+              <v-icon>mdi-chevron-down</v-icon>
+            </v-avatar>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, index) in items" :key="index">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
   </v-app-bar>
 </template>
 
 <script>
 export default {
   name: 'Topbar',
+  data: () => ({
+    UserInfo: {
+      name: 'Seif ElSaeed',
+      photo:
+        'https://images.pexels.com/photos/2444429/pexels-photo-2444429.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    items: [
+      {
+        title: 'Account',
+      },
+      {
+        title: 'Log Out',
+      },
+    ],
+  }),
 };
 </script>
+
+<style scoped>
+.text-user {
+  color: white;
+  font-weight: bold;
+  float: right;
+}
+</style>
