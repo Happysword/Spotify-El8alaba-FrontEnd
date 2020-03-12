bb5cd0293da529bcfc46ba002b4a5588045a6fb3
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2020-03-12 20:20:16 +0200

    Add env vars and config for the testing CLI

--- src/api/server/index.js
+++ src/api/server/index.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
+// HmanA6399: Please use this whenever you want to communicate with the server
+import api from '../../common/config';
+
 export default {
   fetchUsers() {
-  // TODO[@XL3]: Replace this with our server
-    return axios
-      .get('')
-      .then((response) => response.data);
+    // TODO[@XL3]: Replace this with our server
+    return axios.get(api).then((response) => response.data);
   },
 };
