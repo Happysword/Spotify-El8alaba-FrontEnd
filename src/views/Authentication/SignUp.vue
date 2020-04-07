52dfaa83b1d7b2d584fed0fa63190b95708bd204
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2020-04-07 22:06:02 +0200

    Fix FB Signup using static API link

--- src/views/Authentication/SignUp.vue
+++ src/views/Authentication/SignUp.vue
@@ -1,201 +1,186 @@
 <template>
-<v-app>
-<v-content>
-  <v-container id="SignUp_root" class="py-0">
-    <!-- Main row -->
-    <v-row justify="center" align="center">
-      <v-col sm="8" lg="6" xl="4" class="px-8">
-        <!-- Logo -->
-        <router-link to="/">
-          <v-img
-            id="logo"
-            src="../../assets/imgs/El-8alaba.png"
-            contain
-            height="140">
-          </v-img>
-        </router-link>
+  <v-app>
+    <v-content>
+      <v-container id="SignUp_root" class="py-0">
+        <!-- Main row -->
+        <v-row justify="center" align="center">
+          <v-col sm="8" lg="6" xl="4" class="px-8">
+            <!-- Logo -->
+            <router-link to="/">
+              <v-img id="logo" src="../../assets/imgs/El-8alaba.png" contain height="140"></v-img>
+            </router-link>
 
-        <v-col class="text-center">
-          <v-btn id="fbSignupBtn"
-                 color="#1877F2"
-                 rounded
-                 x-large
-                 dark
-                 @click="fbSignup">
-            <v-img
-              src="../../assets/imgs/fb-logo.png"
-              class="mr-4 mt-n1 ml-n4"
-              contain
-              max-height="38"
-              max-width="38">
-            </v-img>
-            Sign Up with Facebook
-          </v-btn>
-        </v-col>
+            <v-col class="text-center">
+              <v-btn id="fbSignupBtn" color="#1877F2" rounded x-large dark @click="fbSignup">
+                <v-img
+                  src="../../assets/imgs/fb-logo.png"
+                  class="mr-4 mt-n1 ml-n4"
+                  contain
+                  max-height="38"
+                  max-width="38"
+                ></v-img>Sign Up with Facebook
+              </v-btn>
+            </v-col>
 
-        <v-row>
-          <v-col>
-            <v-divider class="my-3 mb-6"/>
-          </v-col>
-          <v-col cols="1">
-            <p class="text-center font-weight-bold title"
-              >OR
-            </p>
-          </v-col>
-          <v-col>
-            <v-divider class="my-3 mb-6"/>
-          </v-col>
-        </v-row>
+            <v-row>
+              <v-col>
+                <v-divider class="my-3 mb-6" />
+              </v-col>
+              <v-col cols="1">
+                <p class="text-center font-weight-bold title">OR</p>
+              </v-col>
+              <v-col>
+                <v-divider class="my-3 mb-6" />
+              </v-col>
+            </v-row>
 
-        <!-- Incorrect password bar -->
-        <p
-          id="errorBar"
-          class="caption red darken-1 white--text text-center py-3 mb-8"
-          v-if="userInput.incorrect"
-          >Error. Something went wrong.
-        </p>
+            <!-- Incorrect password bar -->
+            <p
+              id="errorBar"
+              class="caption red darken-1 white--text text-center py-3 mb-8"
+              v-if="userInput.incorrect"
+            >Error. Something went wrong.</p>
 
-        <!-- Form -->
-        <v-form ref="signupForm">
-          <!-- Email -->
-          <v-text-field
-            id="emailField"
-            color="#1DB954"
-            outlined
-            label="Email"
-            v-model="userInput.email"
-            :rules="[
+            <!-- Form -->
+            <v-form ref="signupForm">
+              <!-- Email -->
+              <v-text-field
+                id="emailField"
+                color="#1DB954"
+                outlined
+                label="Email"
+                v-model="userInput.email"
+                :rules="[
               validation.required('Email'),
               validation.minLength('Email', 3),
               validation.validEmail(),
             ]"
-            @change="validateConfirmEmail"
-          />
+                @change="validateConfirmEmail"
+              />
 
-          <!-- Confirm Email -->
-          <v-text-field
-            id="confirmEmailField"
-            color="#1DB954"
-            outlined
-            label="Confirm email"
-            ref="confirmEmail"
-            v-model="userInput.confirmEmail"
-            :rules="[
+              <!-- Confirm Email -->
+              <v-text-field
+                id="confirmEmailField"
+                color="#1DB954"
+                outlined
+                label="Confirm email"
+                ref="confirmEmail"
+                v-model="userInput.confirmEmail"
+                :rules="[
               (data) => (!!data && data === userInput.email)
                 || 'Email address doesn\'t match',
             ]"
-            @change="validateConfirmEmail"
-          />
+                @change="validateConfirmEmail"
+              />
 
-          <!-- Password -->
-          <!-- Minimum password length is 8 -->
-          <v-text-field
-            id="passwordField"
-            color="#1DB954"
-            outlined
-            label="Password"
-            v-model="userInput.password"
-            :rules="[
+              <!-- Password -->
+              <!-- Minimum password length is 8 -->
+              <v-text-field
+                id="passwordField"
+                color="#1DB954"
+                outlined
+                label="Password"
+                v-model="userInput.password"
+                :rules="[
               validation.required('Password'),
               validation.minLength('Password', 8),
             ]"
-            :type="userInput.showPassword ? 'text' : 'password'"
-            :append-icon="userInput.showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append="userInput.showPassword = !userInput.showPassword"
-          />
+                :type="userInput.showPassword ? 'text' : 'password'"
+                :append-icon="userInput.showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="userInput.showPassword = !userInput.showPassword"
+              />
 
-          <!-- What should we call you? -->
-          <v-text-field
-            id="nameField"
-            color="#1DB954"
-            outlined
-            label="Name"
-            placeholder="What should we call you?"
-            v-model="userInput.name"
-            :rules="[
+              <!-- What should we call you? -->
+              <v-text-field
+                id="nameField"
+                color="#1DB954"
+                outlined
+                label="Name"
+                placeholder="What should we call you?"
+                v-model="userInput.name"
+                :rules="[
               validation.required('This field'),
               validation.noSpecialCharacters('This field'),
             ]"
-          />
-
-          <!-- Date of Birth -->
-          <p class="subtitle-2 grey--text text--darken-1">Date of Birth</p>
-          <v-row class="mt-n6 mb-n10">
-            <!-- Day -->
-            <v-col cols="3">
-              <v-text-field
-                id="dobDayField"
-                color="#1DB954"
-                outlined
-                label="Day"
-                v-model="userInput.dob.day"
-                :rules="[validation.validDay()]"
               />
-            </v-col>
-            <!-- Month -->
-            <v-col>
-              <v-select
-                id="dobMonthSelect"
-                color="#1DB954"
-                outlined
-                label="Month"
-                :items="months"
-                v-model="userInput.dob.month"
-                :rules="[validation.required('Month')]"
-              />
-            </v-col>
-            <!-- Year -->
-            <v-col cols="3">
-              <v-text-field
-                id="dobYearField"
-                color="#1DB954"
-                outlined
-                label="Year"
-                v-model="userInput.dob.year"
-                :rules="[validation.validYear()]"
-              />
-            </v-col>
-          </v-row>
 
-          <!-- Gender -->
-          <v-radio-group id="genderRadio" mandatory row v-model="userInput.gender">
-            <v-radio label="Male" value="m"/>
-            <v-radio label="Female" value="f"/>
-          </v-radio-group>
+              <!-- Date of Birth -->
+              <p class="subtitle-2 grey--text text--darken-1">Date of Birth</p>
+              <v-row class="mt-n6 mb-n10">
+                <!-- Day -->
+                <v-col cols="3">
+                  <v-text-field
+                    id="dobDayField"
+                    color="#1DB954"
+                    outlined
+                    label="Day"
+                    v-model="userInput.dob.day"
+                    :rules="[validation.validDay()]"
+                  />
+                </v-col>
+                <!-- Month -->
+                <v-col>
+                  <v-select
+                    id="dobMonthSelect"
+                    color="#1DB954"
+                    outlined
+                    label="Month"
+                    :items="months"
+                    v-model="userInput.dob.month"
+                    :rules="[validation.required('Month')]"
+                  />
+                </v-col>
+                <!-- Year -->
+                <v-col cols="3">
+                  <v-text-field
+                    id="dobYearField"
+                    color="#1DB954"
+                    outlined
+                    label="Year"
+                    v-model="userInput.dob.year"
+                    :rules="[validation.validYear()]"
+                  />
+                </v-col>
+              </v-row>
 
-          <!-- Sign Up -->
-          <v-row justify="center" align="center">
-            <v-btn
-              id="signupBtn"
-              color="#1DB954"
-              rounded
-              dark
-              @click="submit"
-              min-width="60%"
-              x-large
-              >Sign Up
-            </v-btn>
-          </v-row>
-        </v-form>
+              <!-- Gender -->
+              <v-radio-group id="genderRadio" mandatory row v-model="userInput.gender">
+                <v-radio label="Male" value="m" />
+                <v-radio label="Female" value="f" />
+              </v-radio-group>
 
-        <p id="loginPrompt" class="text-center mt-4">
-          Already have an account?
-          <router-link to="/login">
-            <span class="link">Log in</span>
-          </router-link>
-        </p>
+              <!-- Sign Up -->
+              <v-row justify="center" align="center">
+                <v-btn
+                  id="signupBtn"
+                  color="#1DB954"
+                  rounded
+                  dark
+                  @click="submit"
+                  min-width="60%"
+                  x-large
+                >Sign Up</v-btn>
+              </v-row>
+            </v-form>
 
-      </v-col>
-    </v-row>
-  </v-container>
-</v-content>
-</v-app>
+            <p id="loginPrompt" class="text-center mt-4">
+              Already have an account?
+              <router-link to="/login">
+                <span class="link">Log in</span>
+              </router-link>
+            </p>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 import validation from '@/store/modules/auth/validation';
 import cookies from '@/store/modules/auth/cookies';
 import api from 'api-client';
+import apiURL from '../../common/config';
 
 /**
  * @author XL3 <abdelrahman.farid99@eng-st.cu.edu.eg>
@@ -326,7 +311,7 @@ export default {
     },
 
     fbSignup() {
-      window.open('http://localhost:3333/api/v1/authentication/facebook', '_self');
+      window.open(`${apiURL}/api/v1/authentication/facebook`, '_self');
     },
   },
 };
