6dfaf4199852500ddd6a1241c6bf7d56f5967380
naiera-magdy <44544070+naiera-magdy@users.noreply.github.com>
2020-04-11 00:47:11 +0200

    Extra tests for List requests

--- tests/unit/listRequest.spec.js
+++ tests/unit/listRequest.spec.js
@@ -9,21 +9,27 @@ const songlist = songs[2];
 
 describe('Requests of lists', () => {
   it('Get List info request test', async () => {
-    expect.assertions(1);
+    expect.assertions(2);
     const data = await request.fetchList(list.id);
     expect(data).toEqual(list);
+    const data2 = await request.fetchList('1');
+    expect(data2).toEqual({});
   });
 
   it('Get Album info request test', async () => {
-    expect.assertions(1);
+    expect.assertions(2);
     const data = await request.fetchAlbum(Album.id);
     expect(data).toEqual(Album);
+    const data2 = await request.fetchAlbum('1');
+    expect(data2).toEqual({});
   });
 
   it('Get list of songs of list request test', async () => {
-    expect.assertions(1);
+    expect.assertions(2);
     const data = await request.fetchSongs(list.id);
     expect(data).toEqual(songlist.items);
+    const data2 = await request.fetchSongs(1);
+    expect(data2).toEqual({});
   });
 
   it('Get list of songs of album request test', async () => {
@@ -39,12 +45,18 @@ describe('Requests of lists', () => {
   });
 
   it('Check album & playlist save & remove', async () => {
-    expect.assertions(6);
+    expect.assertions(12);
     expect(await request.FollowPlaylist(1)).toEqual({ status: 200 });
     expect(await request.UnfollowPlaylist(1)).toEqual({ status: 200 });
     expect(await request.SaveAlbum(1)).toEqual({ status: 201 });
     expect(await request.RemoveAlbum(1)).toEqual({ status: 200 });
     expect(await request.CheckPlaylist(1)).toEqual({ data: [false] });
     expect(await request.CheckAlbum(1)).toEqual({ data: [false] });
+    expect(await request.FollowPlaylist()).toEqual({ status: 0 });
+    expect(await request.UnfollowPlaylist()).toEqual({ status: 0 });
+    expect(await request.SaveAlbum()).toEqual({ status: 0 });
+    expect(await request.RemoveAlbum()).toEqual({ status: 0 });
+    expect(await request.CheckPlaylist()).toEqual({});
+    expect(await request.CheckAlbum()).toEqual({});
   });
 });
