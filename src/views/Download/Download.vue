3952fdb03a82dfbcf732d4c3234216262dda6474
François Adham <45400238+Francois-Adham@users.noreply.github.com>
2020-05-18 23:45:47 +0200

    updated static views
    - added download button to navbar
    - updated topbar to check if user logged in

--- src/views/Download/Download.vue
+++ src/views/Download/Download.vue
@@ -1,41 +1,51 @@
 <template>
-  <v-container class="cont py-0">
-    <v-row justify="center" align="center">
-        <v-col sm="8" lg="6" xl="4" class="px-8">
-            <v-img
-                max-height="500"
-                max-width="500"
-                contain
-                src="https://open.scdn.co/cdn/images/devices/mac.80dcea83.png">
-            </v-img>
-        </v-col>
-    </v-row>
-    <v-row justify="center" align="center" >
-        <v-col sm="10" lg="8" xl="6" >
-            <p class="white--text display-1 font-weight-bold Download">
-                Download our free app</p>
-            <p class="white--text Download">Listen to the music you love with ease.
-                Download the Spotify El Ghalaba app for your computer.</p>
-            <v-row justify="center" align="center">
-                <v-btn
-                    color="#1DB954"
-                    rounded
-                    dark
-                    large>
-                    DOWNLOAD THE COMPUTER APP
-                </v-btn>
-            </v-row>
-        </v-col>
-    </v-row>
-  </v-container>
+    <v-app style='background: black;'>
+        <v-container class="cont py-0">
+            <top-bar></top-bar>
+            <v-content class="mt-10">
+                <v-row justify="center" align="center">
+                    <v-col sm="8" lg="6" xl="4" class="px-8">
+                        <v-img
+                            max-height="500"
+                            max-width="500"
+                            contain
+                            src="https://open.scdn.co/cdn/images/devices/mac.80dcea83.png">
+                        </v-img>
+                    </v-col>
+                </v-row>
+                <v-row justify="center" align="center" >
+                    <v-col sm="10" lg="8" xl="6" >
+                        <p class="white--text display-1 font-weight-bold Download">
+                            Download our free app</p>
+                        <p class="white--text Download">Listen to the music you love with ease.
+                            Download the Spotify El Ghalaba app for your computer.</p>
+                        <v-row justify="center" align="center">
+                            <v-btn
+                                color="#1DB954"
+                                rounded
+                                dark
+                                large>
+                                DOWNLOAD THE COMPUTER APP
+                            </v-btn>
+                        </v-row>
+                    </v-col>
+                </v-row>
+                </v-content>
+        </v-container>
+    </v-app>
 </template>
 
 <script>
 import analyze from 'rgbaster';
 import EventBus from '../../EventBus';
+import TopBar from '../../components/TopBar.vue';
 
 export default {
   name: 'download',
+  components:
+  {
+    TopBar,
+  },
   async created() {
     console.log('downloading');
     const result = await analyze('https://open.scdn.co/cdn/images/devices/mac.80dcea83.png');
@@ -51,5 +61,6 @@ export default {
 }
 .Download{
     text-align: center;
+    background-color: black;
 }
 </style>
