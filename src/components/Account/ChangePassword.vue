dc11d942da87ff389920f7181268d422077a7a4b
XL3 <abdelrahman.farid99@eng-st.cu.edu.eg>
2020-05-27 17:50:03 +0200

    Added setNotificationSettings to the server API, user module is finished

--- src/components/Account/ChangePassword.vue
+++ src/components/Account/ChangePassword.vue
@@ -1,89 +1,82 @@
 <template>
-<v-app>
-<v-content class="grey lighten-4 pa-0 pa-sm-8">
-<!-- Root Container -->
-<v-container id="EditProfile_root">
-  <p id="title"
-     class="spotify-green headline pt-6 pl-3 mb-12 d-none d-sm-flex">
-    Change your password
-  </p>
+  <v-app>
+    <v-content class="grey lighten-4 pa-0 pa-sm-8">
+      <!-- Root Container -->
+      <v-container id="EditProfile_root">
+        <p id="title" class="spotify-green headline pt-6 pl-3 mb-12 d-none d-sm-flex">
+          Change your password
+        </p>
 
-  <v-card flat id="formCard"
-          class="pa-4 pb-8 pa-sm-12">
-    <!-- Error bar -->
-    <p
-      id="errorBar"
-      class="caption red darken-1 white--text text-center py-3 mb-8"
-      v-if="!!userInput.incorrect">
-      {{ userInput.incorrect }}
-    </p>
+        <v-card flat id="formCard" class="pa-4 pb-8 pa-sm-12">
+          <!-- Error bar -->
+          <p
+            id="errorBar"
+            class="caption red darken-1 white--text text-center py-3 mb-8"
+            v-if="!!userInput.incorrect"
+          >
+            {{ userInput.incorrect }}
+          </p>
 
-    <!-- Form -->
-    <v-form ref="changePasswordForm">
-      <!-- Current password -->
-      <v-text-field id="currentPwField"
-                    color="#1DB954"
-                    outlined
-                    label="Current password"
-                    v-model="userInput.currentPw"
-                    :rules="[
-                      validation.required('Password'),
-                      validation.minLength('Password', 8),
-                    ]"
-                    :type="userInput.showPassword ? 'text' : 'password'"
-                    :append-icon="userInput.showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                    @click:append="userInput.showPassword = !userInput.showPassword"
-      />
-      <!-- New password -->
-      <v-text-field id="newPwField"
-                    color="#1DB954"
-                    outlined
-                    label="New password"
-                    v-model="userInput.newPw"
-                    :rules="[
-                      validation.required('Password'),
-                      validation.minLength('Password', 8),
-                      (data) => (data !== userInput.currentPw) || 'Don\'t use the same password',
-                    ]"
-                    :type="userInput.showPassword ? 'text' : 'password'"
-                    :append-icon="userInput.showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                    @click:append="userInput.showPassword = !userInput.showPassword"
-                    @change="validateRetypeNewPw"
-      />
-      <!-- Retype new password -->
-      <v-text-field id="retypeNewPwField"
-                    color="#1DB954"
-                    outlined
-                    label="Retype new password"
-                    ref="retypeNewPw"
-                    v-model="userInput.retypeNewPw"
-                    :rules="[
-                      (data) => (!!data && data === userInput.newPw)
-                        || 'Password doesn\'t match',
-                    ]"
-                    :type="userInput.showPassword ? 'text' : 'password'"
-                    :append-icon="userInput.showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                    @click:append="userInput.showPassword = !userInput.showPassword"
-      />
+          <!-- Form -->
+          <v-form ref="changePasswordForm">
+            <!-- Current password -->
+            <v-text-field
+              id="currentPwField"
+              color="#1DB954"
+              outlined
+              label="Current password"
+              v-model="userInput.currentPw"
+              :rules="[validation.required('Password'), validation.minLength('Password', 8)]"
+              :type="userInput.showPassword ? 'text' : 'password'"
+              :append-icon="userInput.showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="userInput.showPassword = !userInput.showPassword"
+            />
+            <!-- New password -->
+            <v-text-field
+              id="newPwField"
+              color="#1DB954"
+              outlined
+              label="New password"
+              v-model="userInput.newPw"
+              :rules="[
+                validation.required('Password'),
+                validation.minLength('Password', 8),
+                (data) => data !== userInput.currentPw || 'Don\'t use the same password',
+              ]"
+              :type="userInput.showPassword ? 'text' : 'password'"
+              :append-icon="userInput.showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="userInput.showPassword = !userInput.showPassword"
+              @change="validateRetypeNewPw"
+            />
+            <!-- Retype new password -->
+            <v-text-field
+              id="retypeNewPwField"
+              color="#1DB954"
+              outlined
+              label="Retype new password"
+              ref="retypeNewPw"
+              v-model="userInput.retypeNewPw"
+              :rules="[(data) => (!!data && data === userInput.newPw) || 'Password doesn\'t match']"
+              :type="userInput.showPassword ? 'text' : 'password'"
+              :append-icon="userInput.showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="userInput.showPassword = !userInput.showPassword"
+            />
 
-      <v-row justify="end">
-        <router-link to="/account/overview">
-          <v-btn id="cancelBtn" class="mr-4" text rounded>
-            Cancel
-          </v-btn>
-        </router-link>
-        <v-btn id="saveBtn"
-               class="mr-4" color="#1DB954" rounded dark
-               @click="submit">
-          Set new password
-        </v-btn>
-      </v-row>
-    </v-form>
-  </v-card>
-
-</v-container>
-</v-content>
-</v-app>
+            <v-row justify="end">
+              <router-link to="/account/overview">
+                <v-btn id="cancelBtn" class="mr-4" text rounded>
+                  Cancel
+                </v-btn>
+              </router-link>
+              <v-btn id="saveBtn" class="mr-4" color="#1DB954" rounded dark @click="submit">
+                Set new password
+              </v-btn>
+            </v-row>
+          </v-form>
+        </v-card>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
@@ -125,7 +118,11 @@ export default {
       // and route to Account Overview
       // 200 OK
       if (response.status === 200) {
-        const { data: { data: { user } } } = response;
+        const {
+          data: {
+            data: { user },
+          },
+        } = response;
         // Handling artist objects
         if (user.userInfo) user.email = user.userInfo.email;
         const loginRes = await api.loginUser({
@@ -168,6 +165,6 @@ a {
 }
 
 .spotify-green {
-  color: #1DB954 !important;
+  color: #1db954 !important;
 }
 </style>
